2ee836a7d77073eed3d560a75b574d090114d614
chandananc <chandana@sproutssolutions.com>
2016-04-06 08:12:24 +0000

    changes view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :admin do
     resource :artist_drop_ship_settings
-    resources :shipments
+    resources :artist_shipments
     resources :artists
   end
 end
